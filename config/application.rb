9352975b721a6e00f05a002ed7d2826db3e480df
Rushaine McBean <rushaine.mcbean@gmail.com>
2015-02-07 19:30:28 -0500

    Update gems for dev tools & json rendering

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,11 @@ module Notely
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |generate|
+      generate.test_framework :rspec
+    end
+
+    config.autoload_paths += Dir[Rails.root.join('app', 'serializers', '**/')]
   end
 end
